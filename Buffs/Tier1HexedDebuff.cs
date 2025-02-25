6a42170ff4868af184eaa6acb04705b0e12504d0
Hautaluoma Jimi <jimi.hautaluoma@edu.valkeakoski.fi>
2025-02-25 21:08:33 +0200

    remove 2nd CalamityEncahter folder

---
+++ Buffs/Tier1HexedDebuff.cs
@@ -0,0 +1,36 @@
+using CalamityEnchanter.DamageClasses;
+using CalamityEnchanter.Dusts.Weapons;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityEnchanter.Buffs
+{
+    public class Tier1HexedDebuff : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = true;
+        }
+
+        public override void Update(NPC target, ref int buffIndex)
+        {
+            if (target.buffTime[buffIndex] % 60 == 0)
+            {
+                target.SimpleStrikeNPC(8, 1, false, 0, ModContent.GetInstance<HexDamageClass>());
+            }
+            target.velocity *= 0.9f;
+
+            if (Main.rand.NextBool(3))
+            {
+                Dust.NewDust(
+                    target.position,
+                    target.width,
+                    target.height,
+                    ModContent.DustType<HexDust>()
+                );
+            }
+        }
+    }
+}
