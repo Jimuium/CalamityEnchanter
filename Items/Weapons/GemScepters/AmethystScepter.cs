6c9c0b787b0e9ef548b7d615121a4ab4256e3f6d
Hautaluoma Jimi <jimi.hautaluoma@edu.valkeakoski.fi>
2025-02-26 10:49:49 +0200

    Implement balance to gemscepters and add tooltips to the scepters and their buffs

--- Items/Weapons/GemScepters/AmethystScepter.cs
+++ Items/Weapons/GemScepters/AmethystScepter.cs
@@ -10,24 +10,34 @@ namespace CalamityEnchanter.Items.Weapons.GemScepters
     {
         public override void SetDefaults()
         {
-            Item.width = 64;
-            Item.height = 64;
+            Item.width = 48;
+            Item.height = 48;
             Item.useTime = 60;
-            Item.useAnimation = 20;
+            Item.useAnimation = 30;
             Item.useStyle = ItemUseStyleID.Shoot;
+            Item.autoReuse = true;
 
             Item.value = Item.buyPrice(silver: 2, copper: 40);
             Item.maxStack = 1;
 
             Item.noMelee = true;
             Item.DamageType = ModContent.GetInstance<HexDamageClass>();
-            Item.mana = 20;
-            Item.damage = 20;
+            Item.mana = 28;
+            Item.damage = 12;
             Item.knockBack = 2f;
 
             Item.UseSound = SoundID.Item71;
             Item.shoot = ModContent.ProjectileType<AmethystScepterProjectile>();
-            Item.shootSpeed = 12f;
+            Item.shootSpeed = 10f;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.CopperBar, 12)
+                .AddIngredient(ItemID.Amethyst, 10)
+                .AddTile(TileID.Anvils)
+                .Register();
         }
     }
 }
--- Items/Weapons/GemScepters/EmeraldScepter.cs
+++ Items/Weapons/GemScepters/EmeraldScepter.cs
@@ -10,24 +10,34 @@ namespace CalamityEnchanter.Items.Weapons.GemScepters
     {
         public override void SetDefaults()
         {
-            Item.width = 64;
-            Item.height = 64;
-            Item.useTime = 60;
+            Item.width = 48;
+            Item.height = 48;
+            Item.useTime = 45;
             Item.useAnimation = 20;
             Item.useStyle = ItemUseStyleID.Shoot;
+            Item.autoReuse = true;
 
             Item.value = Item.buyPrice(silver: 2, copper: 40);
             Item.maxStack = 1;
 
             Item.noMelee = true;
             Item.DamageType = ModContent.GetInstance<HexDamageClass>();
-            Item.mana = 20;
-            Item.damage = 20;
-            Item.knockBack = 2f;
+            Item.mana = 22;
+            Item.damage = 18;
+            Item.knockBack = 3f;
 
             Item.UseSound = SoundID.Item71;
             Item.shoot = ModContent.ProjectileType<EmeraldScepterProjectile>();
             Item.shootSpeed = 12f;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.TungstenBar, 12)
+                .AddIngredient(ItemID.Emerald, 10)
+                .AddTile(TileID.Anvils)
+                .Register();
         }
     }
 }
