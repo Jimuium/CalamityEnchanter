2a66ea7bf4019b27b5259cc1bae07d8a4519af7f
Karttunen Kai <kai.karttunen@edu.valkeakoski.fi>
2025-02-27 13:34:15 +0200

    Added stone staff

---
+++ Items/Weapons/StoneStaff.cs
@@ -0,0 +1,42 @@
+using CalamityEnchanter.DamageClasses;
+using CalamityEnchanter.Projectiles.Weapons;
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityEnchanter.Items.Weapons
+{
+    internal class StoneStaff : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 16;
+            Item.height = 16;
+            Item.useStyle = ItemUseStyleID.HoldUp;
+
+            Item.noMelee = true;
+            Item.mana = 2;
+            Item.damage = 0;
+            Item.knockBack = 0f;
+
+            Item.useTime = 30;
+            Item.useAnimation = 30;
+            Item.UseSound = SoundID.Item71;
+            Item.shoot = ModContent.ProjectileType<StoneStaffProjectile>();
+            Item.shootSpeed = 0f;
+            Item.value = Item.buyPrice(silver: 2, copper: 40);
+            Item.maxStack = 1;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().Register();
+        }
+    }
+}
