b45cea8d07cad112c12677bac1c99e29a441ce89
Jimuium <jihautaluoma@gmail.com>
2025-03-05 21:24:46 +0200

    Make all resource types, their ui and damages for them

---
+++ Common/DamageClasses/HexDamageClass.cs
@@ -0,0 +1,20 @@
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CalamityEnchanter.Common.DamageClasses
+{
+    public class HexDamageClass : DamageClass
+    {
+        public override bool GetEffectInheritance(DamageClass damageClass)
+        {
+            // Determines inheritance from other damage types
+            if (damageClass == DamageClass.Generic)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override bool UseStandardCritCalcs => false; // Uses standard crit calculations
+    }
+}
